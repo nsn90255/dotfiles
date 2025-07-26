5fcbfd43381b6a72ec7690ab063651dd731c9bee
nsn90255 <nsn90255@gmail.com>
2025-07-27 00:23:11 +0200

    Add brightness

--- status_bar.sh
+++ status_bar.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 DATE_TIME="$(date +'%Y-%m-%dT%H:%M')"
 BATTERY="B $(cat /sys/class/power_supply/BAT1/capacity)%"
+LUMINOSITY="L $(brightnessctl | awk '/Current brightness/ {print $4}' | sed 's/(//;s/)//')"
 
-echo ${BATTERY} - ${DATE_TIME}
+echo $LUMINOSITY - ${BATTERY} - ${DATE_TIME}
